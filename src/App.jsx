19c687fb4fc03e3b13b37ceef1d3c5e44888d582
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2025-07-08 12:34:57 -0400

    components structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { useRoutes } from "react-router-dom";
+import ShowCreators from "./pages/ShowCreators";
+import ViewCreator from "./pages/ViewCreator";
+import EditCreator from "./pages/EditCreator";
+import AddCreator from "./pages/AddCreator";
+import { BrowserRouter } from "react-router-dom";
+
 function App() {
-  return <></>;
+  const routes = useRoutes([
+    { path: "/", element: <ShowCreators /> },
+    { path: "/creator/:id", element: <ViewCreator /> },
+    { path: "/edit/:id", element: <EditCreator /> },
+    { path: "/new", element: <AddCreator /> },
+  ]);
+
+  return <BrowserRouter>{routes}</BrowserRouter>;
 }
 
 export default App;
